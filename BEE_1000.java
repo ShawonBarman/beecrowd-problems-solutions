831c562d078b26d39cdaf547756317c4ccc555b7
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-06-14 10:44:41 +0600

    Hello World!

---
+++ BEE_1000.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}
