adaa8a32f8b19df2369e764b33734ae6c4ab4a3d
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-06-18 08:58:41 +0600

    Simple Product

---
+++ BEE 1004.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        int a, b;
+        a = input.nextInt();
+        b = input.nextInt();
+        System.out.println("PROD = "+ a*b);
+    }
+}
