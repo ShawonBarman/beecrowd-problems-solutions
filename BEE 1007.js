660cd1e0026e14e70db50cf183d168c2622dbedd
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-06-22 10:03:44 +0600

    Difference

---
+++ BEE 1007.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+var c = parseInt(lines.shift());
+var d = parseInt(lines.shift());
+
+var diff = (a*b)-(c*d);
+
+console.log("DIFERENCA =", diff);
