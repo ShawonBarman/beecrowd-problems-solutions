214fc89bc2dbcd9a3f0e5f2f7b5273608b720be1
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-06-27 07:58:17 +0600

    Area

---
+++ BEE 1012.py
@@ -0,0 +1,6 @@
+a, b, c = map(float, input().split(" "))
+print("TRIANGULO: {:.3f}".format((1.0/2)*a*c))
+print("CIRCULO: {:.3f}".format(3.14159*(c*c)))
+print("TRAPEZIO: {:.3f}".format((1.0/2)*(a+b)*c))
+print("QUADRADO: {:.3f}".format((b*b)))
+print("RETANGULO: {:.3f}".format((a*b)))
