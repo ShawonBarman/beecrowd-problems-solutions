6022a7f9ad53cb0f1443b4d1bf7df53eb42cc121
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-01 14:33:23 +0600

    Distance

---
+++ BEE 1016.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        System.out.println((x*2)+" minutos");
+    }
+}
