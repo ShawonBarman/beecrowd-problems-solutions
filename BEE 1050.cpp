220e9fd448e1662bba7425566c5c59f47bf7eb8b
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-21 16:30:36 +0600

    DDD

---
+++ BEE 1050.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    if (num == 61){
+        cout << "Brasilia" << endl;
+    }
+    else if (num == 71){
+        cout << "Salvador" << endl;
+    }
+    else if (num == 11){
+        cout << "Sao Paulo" << endl;
+    }
+    else if (num == 21){
+        cout << "Rio de Janeiro" << endl;
+    }
+    else if (num == 32){
+        cout << "Juiz de Fora" << endl;
+    }
+    else if (num == 19){
+        cout << "Campinas" << endl;
+    }
+    else if (num == 27){
+        cout << "Vitoria" << endl;
+    }
+    else if (num == 31){
+        cout << "Belo Horizonte" << endl;
+    }
+    else{
+        cout << "DDD nao cadastrado" << endl;
+    }
+    return 0;
+}
