7a0f7cec200841b4b48b151781c65b07b9b400e0
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-21 16:31:25 +0600

    DDD

---
+++ BEE 1050.py
@@ -0,0 +1,20 @@
+num = int(input())
+
+if (num == 61):
+    print("Brasilia")
+elif (num == 71):
+    print("Salvador")
+elif (num == 11):
+    print("Sao Paulo")
+elif (num == 21):
+    print("Rio de Janeiro")
+elif (num == 32):
+    print("Juiz de Fora")
+elif (num == 19):
+    print("Campinas")
+elif (num == 27):
+    print("Vitoria")
+elif (num == 31):
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
