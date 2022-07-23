0c0cc0df41639a84e8c5b62c07120ae232be830e
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-23 09:19:31 +0600

    Month

---
+++ BEE 1052.py
@@ -0,0 +1,26 @@
+num = int(input())
+
+if (num == 1):
+    print("January")
+if (num == 2):
+    print("February")
+if (num == 3):
+    print("March")
+if (num == 4):
+    print("April")
+if (num == 5):
+    print("May")
+if (num == 6):
+    print("June")
+if (num == 7):
+    print("July")
+if (num == 8):
+    print("August")
+if (num == 9):
+    print("September")
+if (num == 10):
+    print("October")
+if (num == 11):
+    print("November")
+if (num == 12):
+    print("December")
