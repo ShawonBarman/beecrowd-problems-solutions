9b3e61e983a1539db414f1c69db7a1f35b1c350a
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-23 09:18:29 +0600

    Month

---
+++ BEE 1052.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        if (num == 1){
+            System.out.println("January");
+        }
+        if (num == 2){
+            System.out.println("February");
+        }
+        if (num == 3){
+            System.out.println("March");
+        }
+        if (num == 4){
+            System.out.println("April");
+        }
+        if (num == 5){
+            System.out.println("May");
+        }
+        if (num == 6){
+            System.out.println("June");
+        }
+        if (num == 7){
+            System.out.println("July");
+        }
+        if (num == 8){
+            System.out.println("August");
+        }
+        if (num == 9){
+            System.out.println("September");
+        }
+        if (num == 10){
+            System.out.println("October");
+        }
+        if (num == 11){
+            System.out.println("November");
+        }
+        if (num == 12){
+            System.out.println("December");
+        }
+    }
+}
