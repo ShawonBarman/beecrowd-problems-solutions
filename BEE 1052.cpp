9912cbe6ad424f32da722cd48cf2284e9496f074
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-23 09:18:10 +0600

    Month

---
+++ BEE 1052.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//january february march april may june july august september october november december
+int main(){
+    int num;
+    cin >> num;
+    if (num == 1){
+        cout << "January" << endl;
+    }
+    if (num == 2){
+        cout << "February" << endl;
+    }
+    if (num == 3){
+        cout << "March" << endl;
+    }
+    if (num == 4){
+        cout << "April" << endl;
+    }
+    if (num == 5){
+        cout << "May" << endl;
+    }
+    if (num == 6){
+        cout << "June" << endl;
+    }
+    if (num == 7){
+        cout << "July" << endl;
+    }
+    if (num == 8){
+        cout << "August" << endl;
+    }
+    if (num == 9){
+        cout << "September" << endl;
+    }
+    if (num == 10){
+        cout << "October" << endl;
+    }
+    if (num == 11){
+        cout << "November" << endl;
+    }
+    if (num == 12){
+        cout << "December" << endl;
+    }
+    return 0;
+}
