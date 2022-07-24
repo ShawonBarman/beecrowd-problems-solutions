16a81b168cf0c0de47bfaf7e892af938a872546a
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-24 23:06:23 +0600

    Even Numbers

---
+++ BEE 1059.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
