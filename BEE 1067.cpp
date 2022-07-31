0cfa7398fab4af52132900b34fb57717b2280b89
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-07-31 07:42:15 +0600

    Odd Numbers

---
+++ BEE 1067.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    for (int i = 1; i <= x; i++){
+        if (i % 2 == 1){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
