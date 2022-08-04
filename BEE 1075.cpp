bfd531d262855edd528be14318d506e8160fc2e0
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-08-04 21:36:43 +0600

    Remaining 2

---
+++ BEE 1075.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i = 2; i < 10000; i++){
+        if (i % n == 2){
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
