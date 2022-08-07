30a93b38cac4d38ecfd4c0e9d4034075d66d1b79
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-08-07 07:35:25 +0600

    Sequence IJ 2

---
+++ BEE 1096.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    for (int i = 1; i < 10; i += 2){
+        for (int j = 7; j > 4; j--){
+            cout << "I=" << i << " J=" << j << endl;
+        }
+    }
+    return 0;
+}
