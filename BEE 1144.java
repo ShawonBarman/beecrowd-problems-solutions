10fe5a8990a66acfcb7ec5c170705dfcd88ff307
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-10-09 12:34:08 +0600

    Logical Sequence

---
+++ BEE 1144.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        for (int i = 1; i <= num; i++){
+            System.out.printf("%d %d %d\n", i, (i * i), (i * i * i));
+            System.out.printf("%d %d %d\n", i, (i * i) + 1, (i * i * i) + 1);
+        }
+    }
+}
