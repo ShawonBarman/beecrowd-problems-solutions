1c7e971f5326f0971974ba477a75c049ef768748
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-10-09 12:32:31 +0600

    Squared and Cubic

---
+++ BEE 1143.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var num = parseInt(lines.shift());
+
+for (var i = 1; i <= num; i++){
+    console.log(i +" "+ (i * i) +" "+ (i * i * i));
+}
