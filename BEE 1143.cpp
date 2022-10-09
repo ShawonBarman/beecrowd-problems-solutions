e90d73507fdb97fedd31819306802b2ffd3da381
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-10-09 12:31:32 +0600

    Squared and Cubic

---
+++ BEE 1143.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    for (int i = 1; i <= num; i++){
+        cout << i << " " << (i * i) << " " << (i * i * i) << endl;
+    }
+    return 0;
+}
