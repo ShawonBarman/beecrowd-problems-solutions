f68c43f0c50d4b390d5156b8d2cc1b7367c258f4
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2023-03-03 11:18:06 +0600

    Delete BEE 1001.js

--- BEE 1001.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-    
-var a = parseInt(lines.shift());
-var b = parseInt(lines.shift());
-
-var sum = a + b;
-
-console.log('X = ' +sum);
