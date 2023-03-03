8beca9c316daa6ed1e2a3bad6d7a7d5eeaff6181
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2023-03-03 11:17:02 +0600

    Area of a Circle

---
+++ BEE 1001.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+    
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+
+var sum = a + b;
+
+console.log('X = ' +sum);
