6a48779c5ffcedbf0cee230d8f6491bb9eb52d8b
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2023-04-26 10:55:36 +0600

    DDD

---
+++ BEE 1050.php
@@ -0,0 +1,32 @@
+<?php
+
+    $ddd = (int)readline();
+    if ($ddd == 61){
+        echo "Brasilia\n";
+    }
+    elseif ($ddd == 71){
+        echo "Salvador\n";
+    }
+    elseif ($ddd == 11){
+        echo "Sao Paulo\n";
+    }
+    elseif ($ddd == 21){
+        echo "Rio de Janeiro\n";
+    }
+    elseif ($ddd == 32){
+        echo "Juiz de Fora\n";
+    }
+    elseif ($ddd == 19){
+        echo "Campinas\n";
+    }
+    elseif ($ddd == 27){
+        echo "Vitoria\n";
+    }
+    elseif ($ddd == 31){
+        echo "Belo Horizonte\n";
+    }
+    else{
+        echo "DDD nao cadastrado\n";
+    }
+
+?>
