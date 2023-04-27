69d686f0c75762d4e8a595364f27509ef6fbee73
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2023-04-27 12:33:49 +0600

    Month

---
+++ BEE 1052.php
@@ -0,0 +1,41 @@
+<?php
+
+    $number = (int)readline();
+    if ($number == 1){
+        echo "January\n";
+    }
+    if ($number == 2){
+        echo "February\n";
+    }
+    if ($number == 3){
+        echo "March\n";
+    }
+    if ($number == 4){
+        echo "April\n";
+    }
+    if ($number == 5){
+        echo "May\n";
+    }
+    if ($number == 6){
+        echo "June\n";
+    }
+    if ($number == 7){
+        echo "July\n";
+    }
+    if ($number == 8){
+        echo "August\n";
+    }
+    if ($number == 9){
+        echo "September\n";
+    }
+    if ($number == 10){
+        echo "October\n";
+    }
+    if ($number == 11){
+        echo "November\n";
+    }
+    if ($number == 12){
+        echo "December\n";
+    }
+
+?>
